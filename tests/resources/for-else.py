87ce0d95424699f1d8e8ffc82a99be01b5355ee3
Martin Eberlein <schuldner-01.vordach@icloud.com>
2024-11-01 17:22:58 +0100

    added test resources

---
+++ tests/resources/for-else.py
@@ -0,0 +1,14 @@
+def find_even(numbers):
+    for num in numbers:
+        if num % 2 == 0:
+            print(f"Found an even number: {num}")
+            break
+    else:
+        print("No even number found.")
+
+
+if __name__ == "__main__":
+    # Test cases
+    find_even([1, 3, 5])      # No even number found.
+    find_even([1, 3, 5, 6])   # Found an even number: 6
+
